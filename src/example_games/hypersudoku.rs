25903d9aaf77c2c34eaa21c7ce30495cbe19c2da
Markus Mayer <widemeadows@gmail.com>
2022-12-11 16:03:32 +0100

    Update docs for Nonomino and Hypersudoku

--- src/example_games/hypersudoku.rs
+++ src/example_games/hypersudoku.rs
@@ -1,6 +1,40 @@
 use crate::prelude::*;
 
 /// Produces an example Hypersudoku game.
+///
+/// Hypersudokus behave like regular Sudoku games with the addition
+/// of four extra groups (the windows) that overlap with the regular
+/// Sudoku blocks.
+///
+/// ## Initial state
+/// ```plain
+///     · · ·   · · ·   · 1 ·
+///     · · 2   · · ·   · 3 4
+///     · · ·   · 5 1   · · ·
+///
+///     · · ·   · · 6   5 · ·
+///     · 7 ·   3 · ·   · 8 ·
+///     · · 3   · · ·   · · ·
+///
+///     · · ·   · 8 ·   · · ·
+///     5 8 ·   · · ·   9 · ·
+///     6 9 ·   · · ·   · · ·
+/// ```
+///
+/// ## Expected solution
+/// ```plain
+///     9 4 6   8 3 2   7 1 5
+///     1 5 2   6 9 7   8 3 4
+///     7 3 8   4 5 1   2 9 6
+///
+///     8 1 9   7 2 6   5 4 3
+///     4 7 5   3 1 9   6 8 2
+///     2 6 3   5 4 8   1 7 9
+///
+///     3 2 7   9 8 5   4 6 1
+///     5 8 4   1 6 3   9 2 7
+///     6 9 1   2 7 4   3 5 8
+/// ```
 #[rustfmt::skip]
 pub fn example_hypersudoku() -> Game {
     let mut groups = CellGroups::default()
@@ -10,27 +44,31 @@ pub fn example_hypersudoku() -> Game {
 
     let x = 0u8;
     let state = GameState::new_from([
-        x, x, x, x, x, x, x, 1, x,
-        x, x, 2, x, x, x, x, 3, 4,
-        x, x, x, x, 5, 1, x, x, x,
-        x, x, x, x, x, 6, 5, x, x,
-        x, 7, x, 3, x, x, x, 8, x,
-        x, x, 3, x, x, x, x, x, x,
-        x, x, x, x, 8, x, x, x, x,
-        5, 8, x, x, x, x, 9, x, x,
-        6, 9, x, x, x, x, x, x, x,
+        x, x, x,  x, x, x,  x, 1, x,
+        x, x, 2,  x, x, x,  x, 3, 4,
+        x, x, x,  x, 5, 1,  x, x, x,
+
+        x, x, x,  x, x, 6,  5, x, x,
+        x, 7, x,  3, x, x,  x, 8, x,
+        x, x, 3,  x, x, x,  x, x, x,
+
+        x, x, x,  x, 8, x,  x, x, x,
+        5, 8, x,  x, x, x,  9, x, x,
+        6, 9, x,  x, x, x,  x, x, x,
     ],);
 
     let solution = GameState::new_from([
-       9, 4, 6, 8, 3, 2, 7, 1, 5,
-       1, 5, 2, 6, 9, 7, 8, 3, 4,
-       7, 3, 8, 4, 5, 1, 2, 9, 6,
-       8, 1, 9, 7, 2, 6, 5, 4, 3,
-       4, 7, 5, 3, 1, 9, 6, 8, 2,
-       2, 6, 3, 5, 4, 8, 1, 7, 9,
-       3, 2, 7, 9, 8, 5, 4, 6, 1,
-       5, 8, 4, 1, 6, 3, 9, 2, 7,
-       6, 9, 1, 2, 7, 4, 3, 5, 8,
+       9, 4, 6,   8, 3, 2,   7, 1, 5,
+       1, 5, 2,   6, 9, 7,   8, 3, 4,
+       7, 3, 8,   4, 5, 1,   2, 9, 6,
+
+       8, 1, 9,   7, 2, 6,   5, 4, 3,
+       4, 7, 5,   3, 1, 9,   6, 8, 2,
+       2, 6, 3,   5, 4, 8,   1, 7, 9,
+
+       3, 2, 7,   9, 8, 5,   4, 6, 1,
+       5, 8, 4,   1, 6, 3,   9, 2, 7,
+       6, 9, 1,   2, 7, 4,   3, 5, 8,
     ],);
 
     Game {
--- src/example_games/nonomino.rs
+++ src/example_games/nonomino.rs
@@ -1,6 +1,39 @@
 use crate::prelude::*;
 
 /// Produces an example Nonomino game.
+///
+/// Unlike regular Sudokus, Nonominos trade the blocks with
+/// nine irregularly shaped regions of nine cells each.
+///
+/// ## Initial state
+/// ```plain
+///     3 · ·   · · ·   · · 4
+///     · · 2   · 6 ·   1 · ·
+///     · 1 ·   9 · 8   · 2 ·
+///
+///     · · 5   · · ·   6 · ·
+///     · 2 ·   · · ·   · 1 ·
+///     · · 9   · · ·   8 · ·
+///
+///     · 8 ·   3 · 4   · 6 ·
+///     · · 4   · 1 ·   9 · ·
+///     5 · ·   · · ·   · · 7
+/// ```
+///
+/// ## Expected solution
+/// ```plain
+///     3 5 8   1 9 6   2 7 4
+///     4 9 2   5 6 7   1 3 8
+///     6 1 3   9 7 8   4 2 5
+///
+///     1 7 5   8 4 2   6 9 3
+///     8 2 6   4 5 3   7 1 9
+///     2 4 9   7 3 1   8 5 6
+///
+///     9 8 7   3 2 4   5 6 1
+///     7 3 4   6 1 5   9 8 2
+///     5 6 1   2 8 9   3 4 7
+/// ```
 #[rustfmt::skip]
 pub fn example_nonomino() -> Game {
     let mut groups = CellGroups::default()
@@ -17,31 +50,31 @@ pub fn example_nonomino() -> Game {
 
     let x = 0u8;
     let state = GameState::new_from([
-        3, x, x,  x, x, x,  x, x, 4,
-        x, x, 2,  x, 6, x,  1, x, x,
-        x, 1, x,  9, x, 8,  x, 2, x,
+        3, x, x,   x, x, x,   x, x, 4,
+        x, x, 2,   x, 6, x,   1, x, x,
+        x, 1, x,   9, x, 8,   x, 2, x,
 
-        x, x, 5,  x, x, x,  6, x, x,
-        x, 2, x,  x, x, x,  x, 1, x,
-        x, x, 9,  x, x, x,  8, x, x,
+        x, x, 5,   x, x, x,   6, x, x,
+        x, 2, x,   x, x, x,   x, 1, x,
+        x, x, 9,   x, x, x,   8, x, x,
 
-        x, 8, x,  3, x, 4,  x, 6, x,
-        x, x, 4,  x, 1, x,  9, x, x,
-        5, x, x,  x, x, x,  x, x, 7,
+        x, 8, x,   3, x, 4,   x, 6, x,
+        x, x, 4,   x, 1, x,   9, x, x,
+        5, x, x,   x, x, x,   x, x, 7,
     ],);
 
     let solution = GameState::new_from([
-        3, 5, 8,  1, 9, 6,  2, 7, 4,
-        4, 9, 2,  5, 6, 7,  1, 3, 8,
-        6, 1, 3,  9, 7, 8,  4, 2, 5,
+        3, 5, 8,   1, 9, 6,   2, 7, 4,
+        4, 9, 2,   5, 6, 7,   1, 3, 8,
+        6, 1, 3,   9, 7, 8,   4, 2, 5,
 
-        1, 7, 5,  8, 4, 2,  6, 9, 3,
-        8, 2, 6,  4, 5, 3,  7, 1, 9,
-        2, 4, 9,  7, 3, 1,  8, 5, 6,
+        1, 7, 5,   8, 4, 2,   6, 9, 3,
+        8, 2, 6,   4, 5, 3,   7, 1, 9,
+        2, 4, 9,   7, 3, 1,   8, 5, 6,
 
-        9, 8, 7,  3, 2, 4,  5, 6, 1,
-        7, 3, 4,  6, 1, 5,  9, 8, 2,
-        5, 6, 1,  2, 8, 9,  3, 4, 7,
+        9, 8, 7,   3, 2, 4,   5, 6, 1,
+        7, 3, 4,   6, 1, 5,   9, 8, 2,
+        5, 6, 1,   2, 8, 9,   3, 4, 7,
     ],);
 
     Game {
